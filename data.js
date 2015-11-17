83e19ca3ce9bc00302f0919c37f93c542f31c2f9
Kayla <ktopper24@gmail.com>
2015-11-17 06:39:10 -0500

    11/17/15

--- data.js
+++ data.js
@@ -14,3 +14,35 @@
    instructions: ["cook chicken", "cook pasta"]
  }
 ]
+
+
+$scope.Italy =
+    {
+      name: "Italy",
+      continent: "Europe",
+      breakfastUrl: "https://breadcakesandale.files.wordpress.com/2013/08/cappuccio-spremuta-lieviti-arabo.jpg",
+      lunchUrl: "http://cdn.assets.cougar.bauer-media.net.au/s3/digital-cougar-assets/WomansDay/2013/11/21/26076/italian-lunch-melt.jpg",
+      dinnerUrl: "http://lalichresources.com/wp-content/uploads/2014/04/Italian-Dinner-700x466.jpg",
+      dessertUrl: "http://gracessweetlife.com/wp-content/uploads/2010/06/ccc3-e1381295500919.jpg"
+    };
+
+  $scope.America=
+  {
+    name: "America",
+    continent: "North America",
+    breakfastUrl: "https://breadcakesandale.files.wordpress.com/2013/08/cappuccio-spremuta-lieviti-arabo.jpg",
+    lunchUrl: "http://cdn.assets.cougar.bauer-media.net.au/s3/digital-cougar-assets/WomansDay/2013/11/21/26076/italian-lunch-melt.jpg",
+    dinnerUrl: "http://lalichresources.com/wp-content/uploads/2014/04/Italian-Dinner-700x466.jpg",
+    dessertUrl: "http://gracessweetlife.com/wp-content/uploads/2010/06/ccc3-e1381295500919.jpg"
+  };
+
+  $scope.Mexico=
+  {
+    name: "Mexico",
+    continent: "North America",
+    breakfastUrl: "http://cook.sndimg.com/content/dam/images/cook/fullset/2011/4/27/0/CC_Chilaquiles_s4x3.jpg/jcr:content/renditions/cq5dam.web.616.462.jpeg",
+    lunchUrl: "https://www.expressentree.com/Images/cuisine/mexican-food-delivery.jpg",
+    dinnerUrl: "http://www.birthdaytrend.com/wp-content/uploads/2013/03/Mexican-Recipes-300x231.jpg",
+    dessertUrl: "http://rampantcuisine.com/wp-content/uploads/2013/11/Churros-294-2.jpg"
+  };
+}]);
